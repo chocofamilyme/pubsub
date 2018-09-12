5e802e968ecefb2bd5afbfac5ef55b021c2fa0ac
docxplusgmoon <ndd_29@mail.ru>
2018-09-12 14:36:12 +0600

    Настройка тестов

---
+++ examples/sub.php
@@ -0,0 +1,20 @@
+<?php
+require_once __DIR__.'/vendor/autoload.php';
+require_once 'functions.php';
+
+use Chocofamily\PubSub\Subscriber;
+
+$params = [
+    'queue_name' => 'book',
+];
+
+$taskName = 'your_task_name';
+
+$subscriber = new Subscriber(getProvider(), 'book.reserved', $params, $taskName);
+
+$subscriber->subscribe(function ($headers, $body) {
+    echo print_r($headers, 1). PHP_EOL;
+    echo print_r($body, 1). PHP_EOL;
+});
+
+\Chocofamily\PubSub\Exceptions\RetryException();
